52f27fbffe3ff8d788a1a5bc81d62782f6d05f49
Jon Chinen <jchinen@dhcp-10-250-250-168.harvard.edu>
2021-10-10 11:20:18 -0400

    Added text labels and updated styling on Legend component.

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -6,25 +6,25 @@ export default function Legend(props) {
     return (
         <div className="Legend">
             <div className="subcontainer">
-                <div className="tribal" style={{backgroundColor: colors.tribal}}/>
+                <div className="tribal" style={{backgroundColor: colors.tribal}}/><span className="detail">Tribal Lands</span>
             </div>
             <div className="subcontainer">
-                <div className="brownfields" style={{backgroundColor: colors.brownfields}}/>
+                <div className="brownfields" style={{backgroundColor: colors.brownfields}}/><span className="detail">Brownfields</span>
             </div>
             <div className="subcontainer">
                 <div className="emptydata" style={{color: colors.emptydata}}>
                     <span>✖</span>
-                </div>
+                </div><span className="detail">Inconclusive Superfunds</span>
             </div>
             <div className="subcontainer">
                 <div className="petroleum" style={{color: colors.petroleum}}>
-                    <span>&#823;</span>
-                </div>
+                    <span>&nbsp;&nbsp;&#823;</span>
+                </div><span className="detail">petroleum pipelines</span>
             </div>
             <div className="subcontainer">
                 <div className="crudeoil" style={{color: colors.crudeoil}}>
-                    <span>&#823;</span>
-                </div>
+                    <span>&nbsp;&nbsp;&#823;</span>
+                </div><span className="detail">Crudeoil Pipelines</span>
             </div>
         </div>
     )
